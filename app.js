669bbd2aca494bd610b62e5b91f49943ace08e48
Hemil <marcosisgod0585@gmail.com>
2023-07-29 21:27:34 +0530

    Update app.js
    ^

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const newsTitleEl = document.getElementById('newsTitle');
-const newsDescEl = document.getElementById('newsDesc');
-const newsAuthorEl = document.getElementById('newsAuthor');
-const searchBtnEl = document.getElementById('searchBtn');
-const topicEl = document.getElementById('topic');
-const containerEl = document.querySelector('.container');
+const newsTitleEl = document.getElementById("newsTitle");
+const newsDescEl = document.getElementById("newsDesc");
+const newsAuthorEl = document.getElementById("newsAuthor");
+const searchBtnEl = document.getElementById("searchBtn");
+const topicEl = document.getElementById("topic");
+const containerEl = document.querySelector(".container");
 
 function clearNews() {
   while (containerEl.firstChild) {
@@ -11,17 +11,17 @@ function clearNews() {
   }
 }
 
-searchBtnEl.addEventListener('click', (event) => {
+searchBtnEl.addEventListener("click", (event) => {
   clearNews();
 
   const value = topicEl.value;
-  const apiKey = 'My API Key';
+  const apiKey = "My API Key";
   const apiUrl = `https://newsapi.org/v2/top-headlines?q=${value}&apiKey=${apiKey}`;
 
   fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
-        throw new Error('Data not found');
+        throw new Error("Data not found");
       }
       return response.json();
     })
@@ -29,43 +29,43 @@ searchBtnEl.addEventListener('click', (event) => {
       console.log(data);
 
       if (data.articles.length === 0) {
-        alert('No data available for this topic');
+        alert("No data available for this topic");
       } else {
         for (let i = 0; i < data.articles.length; i++) {
           let myData = data.articles[i];
 
-          let box = document.createElement('div');
-          box.className = 'box';
+          let box = document.createElement("div");
+          box.className = "box";
 
-          let img = document.createElement('img');
-          img.setAttribute('src', myData.urlToImage);
+          let img = document.createElement("img");
+          img.setAttribute("src", myData.urlToImage);
           img.setAttribute(
-            'alt', 
-            "Missing image, but don\'t worry - the content is still great"
+            "alt",
+            "Missing image, but don't worry - the content is still great"
           );
-          img.setAttribute('width', '300');
-          img.setAttribute('height', '150');
-          img.style.borderRadius = '20px';
+          img.setAttribute("width", "300");
+          img.setAttribute("height", "150");
+          img.style.borderRadius = "20px";
 
-          let title = document.createElement('h2');
-          title.id = 'newsTitle';
-          title.style.fontFamily = 'Montserrat';
+          let title = document.createElement("h2");
+          title.id = "newsTitle";
+          title.style.fontFamily = "Montserrat";
 
-          let desc = document.createElement('h4');
-          desc.id = 'newsDesc';
-          desc.style.fontFamily = 'Open Sans';
+          let desc = document.createElement("h4");
+          desc.id = "newsDesc";
+          desc.style.fontFamily = "Open Sans";
 
-          let author = document.createElement('h5');
-          author.id = 'newsAuthor';
-          author.style.fontFamily = 'Open Sans';
-          author.style.fontWeight = '400';
+          let author = document.createElement("h5");
+          author.id = "newsAuthor";
+          author.style.fontFamily = "Open Sans";
+          author.style.fontWeight = "400";
 
-          let hr = document.createElement('hr');
+          let hr = document.createElement("hr");
 
-          let viewMoreBtn = document.createElement('button');
-          viewMoreBtn.id = 'viewMoreBtn';
-          viewMoreBtn.textContent = 'Read More';
-          viewMoreBtn.addEventListener('click', (event) => {
+          let viewMoreBtn = document.createElement("button");
+          viewMoreBtn.id = "viewMoreBtn";
+          viewMoreBtn.textContent = "Read More";
+          viewMoreBtn.addEventListener("click", (event) => {
             window.open(myData.url);
           });
 
@@ -74,17 +74,17 @@ searchBtnEl.addEventListener('click', (event) => {
 
           title.textContent = myData.title;
           desc.textContent = myData.description;
-          author.textContent = '-' + myData.author;
+          author.textContent = "-" + myData.author;
 
-          if (desc.textContent == '') {
+          if (desc.textContent == "") {
             desc.textContent =
-              "We\'re sorry, but the content you requested could not be loaded at this time.";
+              "We're sorry, but the content you requested could not be loaded at this time.";
           }
         }
       }
     })
     .catch((error) => {
       console.error(error);
-      alert('Data not found');
+      alert("Data not found");
     });
 });
